349724cffce8644d5d1169b85eada9bfb3954987
Billy Duartez <duartezb@knights.ucf.edu>
2021-09-05 14:55:00 -0400

    pseudocode

--- src/main/java/Solution04.java
+++ src/main/java/Solution04.java
@@ -6,6 +6,7 @@
 /*A simple mad-lib program that prompts for a noun, a verb, an adverb,
  and an adjective and injects those into a story that you create*/
 
+/*import plug libraries*/
 import java.util.Scanner;
 import java.lang.String;
 
@@ -13,24 +14,24 @@ public class Solution04 {
 
     public static void main(String[] args) {
 
-        //enter noun
+        /*prompt message to enter noun*/
         System.out.printf("Enter a noun: ");
         Scanner input = new Scanner(System.in);
         String noun = input.nextLine();
 
-        //enter verb
+        /*prompt message to enter verb*/
         System.out.printf("Enter a verb: ");
         String verb = input.nextLine();
 
-        //enter adverb
+        /*prompt message to enter adverb*/
         System.out.printf("Enter an adverb: ");
         String adverb = input.nextLine();
 
-        //enter adjective
+        /*prompt message to enter adjective*/
         System.out.printf("Enter an adjective: ");
         String adj = input.nextLine();
 
-        //repeat back to user
+        /*repeat back to user*/
         System.out.printf("Do you "+verb+" your "+ adj + " " + noun + " " + adverb + " ? That's hilarious!");
 
 
